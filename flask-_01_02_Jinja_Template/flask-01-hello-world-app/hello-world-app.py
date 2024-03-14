65b4f05df86564cfc7e266bc5f397dda636217f7
polat29 <beratalha29@gmail.com>
2024-03-14 11:12:16 +0100

    ilk portfolio yapildi

---
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/") # ana sayfayi temsil eder
+def head ():
+     return "hello word ramazan"
+@app.route("/secondpage")
+def second():
+     return "this is second page"
+
+@app.route('/fourth/<string:id>')
+def fourth(id):
+     return f'Id of this page is {id}'
+
+
+
+
+
+if __name__ == '__main__':
+
+    #app.run(debug=True)
+    app.run(host= '0.0.0/0', port=80)
